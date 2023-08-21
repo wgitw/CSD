bc9cc4bee3e24d40cab239557324c1e53b900761
gahyeon <1000ji1@naver.com>
2023-08-21 18:31:37 +0900

    Feat/Combine silent sounds

--- CSDServer/CSDServer/views.py
+++ CSDServer/CSDServer/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 import traceback
 import os
+from pydub import AudioSegment
 
 
 @csrf_exempt
@@ -14,6 +15,14 @@ def process_audio(request):
             print("POST")
             # POST 요청에서 이미지 파일을 가져옵니다.
             m4a_file = request.FILES['m4a']
+
+            # 소리 + 묵음
+            # load the audio files
+            audio1 = AudioSegment.from_file(m4a_file, format="m4a")
+            audio2 = AudioSegment.from_file("slient.m4a", format="m4a")
+
+            # concatenate the audio files
+            combined_audio = audio1 + audio2
 
             # 성공적으로 파일을 받았을 때 200 OK 응답을 반환합니다.
             return HttpResponse(status=200)

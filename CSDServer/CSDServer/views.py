9f7ac50f1a9050a2ea28c9ea5629c4e4448d1250
gahyeon <1000ji1@naver.com>
2023-08-23 19:31:37 +0900

    Feat/Convert to frequency-time domain using librosa.stft function

--- CSDServer/CSDServer/views.py
+++ CSDServer/CSDServer/views.py
@@ -1,11 +1,16 @@
+import traceback
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
 from django.http import HttpResponse, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
-import requests
-import traceback
-import os
 from pydub import AudioSegment
-import librosa, librosa.display
 
+FIG_SIZE = (15, 10)
+DATA_NUM = 30
 
 # m4a -> wav -> spectrogram
 @csrf_exempt
@@ -54,6 +59,33 @@ def process_audio(request):
                 print(peekIndex)
                 startPoint = peekIndex
                 endPoint = peekIndex + END_LEN
+
+            # 단순 푸리에 변환 -> Specturm
+            fft = np.fft.fft(sig[startPoint:endPoint])
+            # 복소공간 값 절댓갑 취해서, magnitude 구하기
+            magnitude = np.abs(fft)
+            # Frequency 값 만들기
+            f = np.linspace(0, sr, len(magnitude))
+            # 푸리에 변환을 통과한 specturm은 대칭구조로 나와서 high frequency 부분 절반을 날려고 앞쪽 절반만 사용한다.
+            left_spectrum = magnitude[:int(len(magnitude) / 2)]
+            left_f = f[:int(len(magnitude) / 2)]
+            # STFT -> Spectrogram
+            hop_length = 512  # 전체 frame 수
+            n_fft = 2048  # frame 하나당 sample 수
+            # calculate duration hop length and window in seconds
+            hop_length_duration = float(hop_length) / sr
+            n_fft_duration = float(n_fft) / sr
+
+            # STFT
+            stft = librosa.stft(sig[startPoint:endPoint], n_fft=n_fft, hop_length=hop_length)
+            # 복소공간 값 절댓값 취하기
+            magnitude = np.abs(stft)
+            # magnitude > Decibels
+            log_spectrogram = librosa.amplitude_to_db(magnitude)
+            FIG_SIZE = (10, 10)
+            # display spectrogram
+            plt.figure(figsize=FIG_SIZE)
+            librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length, cmap='magma')
 
 
 
